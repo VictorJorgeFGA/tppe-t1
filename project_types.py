1e4bc4bdb7d131ce5d17dc6dc0c5d8554899718a
Joao Henrique <joao.henrique1299@hotmail.com>
2023-05-18 13:22:19 -0300

    (Falsifie) get_completude for class Name always return True

--- project_types.py
+++ project_types.py
@@ -13,3 +13,49 @@ class Leaf:
         Obtain if leaf is complete or not.
         """
         return True if len(self.value) > 0 else False
+
+class Name:
+    def __init__(self, first_name: str = "", middle_name: str = "", last_name :str = "", type_completude: str = "or"):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+
+        self.type_completude = type_completude
+
+    def get_completude(self):
+        return True
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        self._first_name = Leaf("FirstName",value)
+
+    @property
+    def middle_name(self):
+        return self._middle_name
+
+    @middle_name.setter
+    def middle_name(self, value):
+        self._middle_name = Leaf("MiddleName",value)
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        self._last_name = Leaf("LastName",value)
+
+    @property
+    def type_completude(self):
+        return self._type_completude
+
+    @type_completude.setter
+    def type_completude(self, value):
+        if value in "xor":
+            self._type_completude = value
+        else:
+            raise ValueError("Type completude must be one of the following options: xor, or.")
