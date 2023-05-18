6fe75738b2d8c5c2a1a4cf66887854ac5d9b8b38
Joao Henrique <joao.henrique1299@hotmail.com>
2023-05-18 13:29:14 -0300

    (Triangulation) Create bunch of tests for get_completude of class Name.

--- test_get_completude.py
+++ test_get_completude.py
@@ -27,3 +27,23 @@ from project_types import Leaf, Name
 ])
 def test_get_completute_leaf(element: Leaf, is_complete: bool):
     assert element.get_completude() == is_complete
+
+@pytest.mark.parametrize("name, is_complete",[
+    (Name("Andre","Santos","Rodrigues","or"),True),
+    (Name("Andre","Santos","Rodrigues","xor"),True),
+
+    (Name("","","","xor"),False),
+    (Name("","","","or"),False),
+
+    (Name("Andre","","","or"),True),
+    (Name("","Santos","","or"),True),
+    (Name("","","Rodrigues","or"),True),
+
+    (Name("Andre","","","xor"),True),
+    (Name("","Santos","","xor"),True),
+
+    (Name("Andre","Santos","","xor"),False),
+    (Name("Andre","","Rodrigues","xor"),False),
+])
+def test_get_completute_name(name: Name, is_complete: bool):
+    assert name.get_completude() == is_complete
