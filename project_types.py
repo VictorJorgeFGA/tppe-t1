dfd51fb18fcd53167a2379f8d74254522dd62a05
Joao Henrique <joao.henrique1299@hotmail.com>
2023-05-17 23:48:58 -0300

    Create class for atomic attributes

---
+++ project_types.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+class leaf:
+    """
+    Basic data type for analise if an atribute is complete.
+    """
+    def __init__(self, name: str = "", value: str = ""):
+        self.name = name
+        self.value = value
