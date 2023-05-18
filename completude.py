7941081a6d216f61485a9925831ef0912bc920a7
Joao Henrique <joao.henrique1299@hotmail.com>
2023-05-18 00:00:57 -0300

    (Triangulation) Set of tests for leaf class.

--- completude.py
+++ completude.py
@@ -2,4 +2,4 @@ from project_types import leaf
 
 
 def get_completude_leaf(element: leaf) -> bool:
-    return True
+    return True if len(element.value) > 0 else False
--- test_get_completude.py
+++ test_get_completude.py
@@ -1,71 +1,30 @@
-import completude
-import json
-import unittest
+import pytest
+
+from completude import get_completude_leaf
+from project_types import leaf
 
 
-class test_get_completude(unittest.TestCase):
-    def test(self):
-        test_cases = [
-            {
-                "campos":  {
-                    "nome": "nome1",
-                    "tipo": "OU EXCLUSIVO",
-                    "atributos": [
-                            {
-                                "nome": "nome2",
-                                "tipo": "OU INCLUSIVO",
-                                "atributos": []
-                            },
-                        {
-                                "nome": "nome3",
-                                "tipo": "OU INCLUSIVO",
-                                "atributos": []
-                            }
-                    ]
-                },
-                "item": {
-                    "nome": "nome1",
-                    "valores": [
-                        {
-                            "nome": "nome2",
-                            "valor": "vitor",
-                            "atributos": []
-                        },
-                        {
-                            "nome": "nome3",
-                            "valor": "pedro",
-                            "atributos": []
-                        },
-                    ]
-                },
-                "resposta_esperada": {
-                    "nome": "nome1",
-                    "tipo": "OU EXCLUSIVO",
-                    "completo": False,
-                    "valores": [
-                        {
-                            "nome": "nome2",
-                            "tipo": "OU INCLUSIVO",
-                            "completo": True,
-                            "valor": "vitor"
-                        },
-                        {
-                            "nome": "nome3",
-                            "tipo": "OU INCLUSIVO",
-                            "completo": True,
-                            "valor": "pedro"
+@pytest.mark.parametrize("element, is_complete",[
+    (leaf("CPF","111.111.111-11"),True),
+    (leaf("CPF",""),False),
 
-                        }
-                    ]
-                }
-            }
-        ]
+    (leaf("Matricula","11/1111111"),True),
+    (leaf("Matricula",""),False),
 
-        for case in test_cases:
-            resposta = completude.get_completude(case["item"], case["campos"])
-            print("teste passou!")
-            self.assertDictEqual(resposta, case["resposta_esperada"])
+    (leaf("Sexo","M"),True),
+    (leaf("Sexo",""),False),
 
+    (leaf("Email","jorge@email.com"),True),
+    (leaf("Email",""),False),
 
-if __name__ == "__main__":
-    unittest.main()
+    (leaf("PrimeiroNome","Andre"),True),
+    (leaf("PrimeiroNome",""),False),
+
+    (leaf("NomeMeio","Rodrigues"),True),
+    (leaf("NomeMeio",""),False),
+
+    (leaf("UltimoNome","Santos"),True),
+    (leaf("UltimoNome",""),False),
+])
+def test_get_completute_leaf(element: leaf, is_complete: bool):
+    assert get_completude_leaf(element) == is_complete
