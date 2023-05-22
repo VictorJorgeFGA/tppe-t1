a7a231c43040e2d83d100fa03989547f208cfc06
Victor Gonçalves <victor.eng.unb@gmail.com>
2023-05-22 17:01:04 -0300

    creating duplicate for getting green on test cases

--- completude.rb
+++ completude.rb
@@ -1,5 +1,9 @@
 module Completude
   def self.get_completude(json)
-    !json.values.first.empty?
+    if json.values.size == 1
+      !json.values.first.empty?
+    else
+      (!json.values.first.empty?) || (!json.values[1].empty?)
+    end
   end
 end
