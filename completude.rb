757abcf3695664b9b3f783d22d059e25ad131ace
Victor Gonçalves <victor.eng.unb@gmail.com>
2023-05-22 20:08:15 -0300

    refactor completude to cover nested attributes verification

--- completude.rb
+++ completude.rb
@@ -1,7 +1,19 @@
 module Completude
+  def self.is_empty?(h)
+    if h.is_a?(Hash)
+      ans = true
+      h.keys.each do |k|
+        ans = ans && is_empty?(h[k])
+      end
+      ans
+    else
+      h.empty?
+    end
+  end
+
   def self.get_completude(json, method = :or)
     is_present = -> (a) do
-      !a.empty?
+      !is_empty?(a)
     end
 
     if method == :or
