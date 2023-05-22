baaa9539a24cfa4e0c2928bd7003f5c6872f98ac
Victor Gonçalves <victor.eng.unb@gmail.com>
2023-05-22 17:27:47 -0300

    refactoring get_completude_function to solve jsons with n atomic attributes

--- completude.rb
+++ completude.rb
@@ -1,11 +1,5 @@
 module Completude
   def self.get_completude(json)
-    if json.values.size == 1
-      !json.values.first.empty?
-    elsif json.values.size == 2
-      (!json.values.first.empty?) || (!json.values[1].empty?)
-    else
-      (!json.values.first.empty?) || (!json.values[1].empty?) || (!json.values[2].empty?)
-    end
+    json.values.any? { |value| !value.empty? }
   end
 end
