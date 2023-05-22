e69a7b7dd26f6a31cc5aeba50cb5947a08ca6d9e
Joao Henrique <joao.henrique1299@hotmail.com>
2023-05-22 17:06:41 -0300

    adding another duplicate

--- completude.rb
+++ completude.rb
@@ -2,8 +2,10 @@ module Completude
   def self.get_completude(json)
     if json.values.size == 1
       !json.values.first.empty?
-    else
+    elsif json.values.size == 2
       (!json.values.first.empty?) || (!json.values[1].empty?)
+    else
+      (!json.values.first.empty?) || (!json.values[1].empty?) || (!json.values[2].empty?)
     end
   end
 end
