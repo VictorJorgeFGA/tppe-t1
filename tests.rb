aa0e438cf5cd41d6e38691b4c9852959ab97aab6
Victor Gonçalves <victor.eng.unb@gmail.com>
2023-05-22 16:40:28 -0300

    creating tests for get_completude

---
+++ tests.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'byebug'
+require 'pry'
+
+load 'completude.rb'
+
+class FeatureTests < Minitest::Test
+
+  def self.atomic_test_cases
+    [
+      {'parameters' => {'matricula' => '18000000'}, 'answer' => true},
+      {'parameters' => {'matricula' => ''}, 'answer' => false}
+    ]
+  end
+
+  atomic_test_cases.each_with_index do |test_case, idx|
+    define_method "test_atomic_completude_#{idx}" do
+      assert_equal test_case['answer'], Completude::get_completude(test_case['parameters'])
+    end
+  end
+end
