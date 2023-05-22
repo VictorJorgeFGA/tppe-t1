bde1c117836ffde939cab843a8e371e87c110a1f
Joao Henrique <joao.henrique1299@hotmail.com>
2023-05-22 17:49:48 -0300

    adding xor to completude check

--- completude.rb
+++ completude.rb
@@ -1,5 +1,13 @@
 module Completude
-  def self.get_completude(json)
-    json.values.any? { |value| !value.empty? }
+  def self.get_completude(json, method = :or)
+    is_present = -> (a) do
+      !a.empty?
+    end
+
+    if method == :or
+      json.values.any?(&is_present)
+    else
+      json.values.map(&is_present).reduce(false) { |a,b| a ^ b }
+    end
   end
 end
