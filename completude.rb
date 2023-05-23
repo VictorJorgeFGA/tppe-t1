60970292d032a4898b251051417b5fb181c6a6ad
Victor Gonçalves <victor.eng.unb@gmail.com>
2023-05-22 21:07:31 -0300

    refactor completude to cover or array cases

--- completude.rb
+++ completude.rb
@@ -3,6 +3,8 @@ module Completude
     h.keys.each do |k|
       if h[k].is_a?(Hash)
         presence(h[k])
+      elsif h[k].is_a?(Array)
+        h[k] = h[k].map { |item| !item.empty? }
       else
         h[k] = !h[k].empty?
       end
@@ -14,6 +16,22 @@ module Completude
     h.values.each do |v|
       if v.is_a?(Hash)
         ans = yield(ans, func(v, &block))
+      elsif v.is_a?(Array)
+        ans = yield(ans, func_arr(v, &block))
+      else
+        ans = yield(ans, v)
+      end
+    end
+    ans
+  end
+
+  def self.func_arr(arr, &block)
+    ans = false
+    arr.each do |v|
+      if v.is_a?(Hash)
+        ans = yield(ans, func(v, &block))
+      elsif v.is_a?(Array)
+        ans = yield(ans, func_arr(v, &block))
       else
         ans = yield(ans, v)
       end
@@ -22,6 +40,7 @@ module Completude
   end
 
   def self.get_completude(json, method = :or)
+
     presence(json)
 
     if method == :or
